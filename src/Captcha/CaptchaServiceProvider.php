ca3879ec12917c04fb1c3ae1d4e63c1c90471e77
wanganlin <inbox@wanganlin.com>
2024-07-16 21:35:44 +0800

    更新图片验证码配置文件

--- src/Captcha/CaptchaServiceProvider.php
+++ src/Captcha/CaptchaServiceProvider.php
@@ -11,14 +11,14 @@ class CaptchaServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->mergeConfigFrom(
-            dirname(__DIR__).'/Config/captcha.php', 'captcha'
+            __DIR__.'/Config/captcha.php', 'captcha'
         );
     }
 
     public function boot(): void
     {
         $this->publishes([
-            dirname(__DIR__).'/Config/captcha.php' => config_path('captcha.php'),
+            __DIR__.'/Config/captcha.php' => config_path('captcha.php'),
         ]);
 
         $this->loadRoutesFrom(__DIR__.'/Routes/web.php');
